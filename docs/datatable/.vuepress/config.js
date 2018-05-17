8e470d1c58e325917b40ad94b4d104b2c157c25b
Faris Ansari <netchamp.faris@gmail.com>
2018-05-17 17:12:58 +0530

    fix base

--- docs/datatable/.vuepress/config.js
+++ docs/datatable/.vuepress/config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     title: 'Frappe DataTable',
     description: 'A simple, modern and interactive datatable for the web',
-    base: '/docs/datatable',
+    base: '/docs/datatable/',
     dest: path.resolve('./static/docs/datatable'),
     themeConfig: {
         sidebar: [
